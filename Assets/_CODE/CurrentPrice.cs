35fa208480b583629472c165e03b12124316c053
Zihao Feng <zihao@artandalchemy.io>
2015-12-16 16:59:10 -0500

    Initial commit

---
+++ Assets/_CODE/CurrentPrice.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CurrentPrice : MonoBehaviour {
+
+    [Header("Prices")]
+    public float currentPrice = 15000;
+    public float lowestPrice = 13000;
+    public Text priceText;
+    
+	void Start ()
+    {
+        priceText = GetComponent<Text>() as Text;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        currentPrice -= Time.deltaTime;
+        priceText.text = "Current Price: " + (int)currentPrice;
+	
+	}
+}
